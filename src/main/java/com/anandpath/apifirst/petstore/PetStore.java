127b46352e7311df7511847c853fbc749a40fe20
Anand Pathak <anandpath@gmail.com>
2022-01-09 22:28:53 +0530

    Updated for basic build elements, sample api yaml and bootstrap.yaml

---
+++ src/main/java/com/anandpath/apifirst/petstore/PetStore.java
@@ -0,0 +1,11 @@
+package com.anandpath.apifirst.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStore {
+    public static void main(String[] args) {
+        SpringApplication.run(PetStore.class, args);
+    }
+}
